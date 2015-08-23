563a4e22ba6a6bfdb857886375135c7ea8590d83
lsecrease <lsecrease@gmail.com>
2015-08-23 00:44:51 -0500

    …

---
+++ Cluster/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  Cluster
+//
+//  Created by lsecrease on 8/19/15.
+//  Copyright (c) 2015 ImagineME. All rights reserved.
+//
+
+import UIKit
+
+var test = 10
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
