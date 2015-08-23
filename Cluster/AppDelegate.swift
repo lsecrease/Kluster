197719a186a1a5b16832b8f9dd795716c30dbed6
lsecrease <lsecrease@gmail.com>
2015-08-23 15:21:28 -0500

    Added Bridging Header/Frameworks

--- Cluster/AppDelegate.swift
+++ Cluster/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        Parse.setApplicationId(“yourapplicationkey”, clientKey: “yourclientkey”)
+        
+        
         return true
     }
 
