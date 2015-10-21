a03c364a72c70712707a8169d3f9e5d58ae709f4
lsecrease <lsecrease@gmail.com>
2015-10-20 21:06:10 -0500

    Kluster Group Screens

---
+++ Cluster/KlusterHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  KlusterHeaderView.swift
+//  Cluster
+//
+//  Created by lsecrease on 10/20/15.
+//  Copyright (c) 2015 ImagineME. All rights reserved.
+//
+
+import UIKit
+
+class KlusterHeaderView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
