dd22150c03a27f581d0a80f67e0d53aaedf1b3dc
lsecrease <lawrenceolivier@Lawrences-MBP.attlocal.net>
2015-11-15 16:26:04 -0600

    KlusterInvite/Notification UI

---
+++ Cluster/KlusterInviteTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  KlusterInviteTableViewCell.swift
+//  Cluster
+//
+//  Created by Lawrence Olivier on 11/15/15.
+//  Copyright © 2015 ImagineME. All rights reserved.
+//
+
+import UIKit
+
+class KlusterInviteTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var inviteButton: DesignableButton!
+    
+
+}
---
+++ Cluster/KlusterInviteViewController.swift
@@ -0,0 +1,27 @@
+//
+//  KlusterInviteViewController.swift
+//  Cluster
+//
+//  Created by Lawrence Olivier on 11/15/15.
+//  Copyright © 2015 ImagineME. All rights reserved.
+//
+
+import UIKit
+
+class KlusterInviteViewController: UIViewController {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var inviteButton: DesignableButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+   
+
+}
