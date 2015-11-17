f56d7f258e6c83905d3a3b0783dbe245eebb1c6e
lsecrease <lawrenceolivier@Champ-MBP.attlocal.net>
2015-11-16 23:28:45 -0600

    Notification/Map(Location)/MyKluster UI in Storyboard

---
+++ Cluster/NotificationsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationsTableViewCell.swift
+//  Cluster
+//
+//  Created by Lawrence Olivier on 11/16/15.
+//  Copyright © 2015 ImagineME. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Cluster/NotificationsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationsViewController.swift
+//  Cluster
+//
+//  Created by Lawrence Olivier on 11/16/15.
+//  Copyright © 2015 ImagineME. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
